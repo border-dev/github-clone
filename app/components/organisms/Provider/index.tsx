0232937a239349325ca5e3597a7184cdb5cb70e4
morgnism <morgan@border.dev>
2023-06-21 18:45:29 -0600

    added react-query and devtools

--- app/components/organisms/Provider/index.tsx
+++ app/components/organisms/Provider/index.tsx
@@ -1,9 +1,20 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
 
+const queryClient = new QueryClient();
+
 const Provider = ({ children }: React.PropsWithChildren) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 };
 
 export default Provider;
