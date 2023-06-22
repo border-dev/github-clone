5a124bd596ceb8f0da18f9364984ae3c821375e0
morgnism <morgan@border.dev>
2023-06-22 14:05:58 -0600

    added graphql codegen and github schema

--- app/components/organisms/Provider/index.tsx
+++ app/components/organisms/Provider/index.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/app/lib/query-client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
-
-const queryClient = new QueryClient();
 
 const Provider = ({ children }: React.PropsWithChildren) => {
   return (
