091ecdb489d841be3e592eb4dcd146b37279595a
morgnism <morgan@border.dev>
2023-06-23 22:32:27 -0600

    chore: renamed providers file and defined query client defaults

--- src/components/organisms/Providers/index.tsx
+++ src/components/organisms/Providers/index.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@lib/query-client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
-
-const queryClient = new QueryClient();
 
 const Providers = ({ children }: React.PropsWithChildren) => {
   return (
