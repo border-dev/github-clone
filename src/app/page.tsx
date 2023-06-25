2bbf8658e2dc747f3ec06648d6e24addaaf030db
Morgan Worrell <morgan@border.dev>
2023-06-25 17:45:31 -0600

    feat: cleaned up home page with smaller components
    * feat: added home page feed footer component
    
    * feat: added home left side nav component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,10 @@
 'use client';
 
 import Icon from '@components/atoms/Icon';
-import Link from '@components/modules/Link';
-import UserTopRepos from '@components/modules/UserTopRepos';
+import HomeFeedFooter from '@components/organisms/HomeFeedFooter';
+import HomeLeftSidebar from '@components/organisms/HomeLeftSidebar';
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
-
-const footerLinks: any[] = [
-  { title: 'Blog', url: 'https://github.blog/' },
-  { title: 'About', url: 'https://github.com/about' },
-  { title: 'Shop', url: 'https://shop.github.com/' },
-  {
-    title: 'Contact Github',
-    url: 'https://support.github.com/?tags=dotcom-footer',
-  },
-  { title: 'Pricing', url: 'https://github.com/pricing' },
-
-  { title: 'API', url: 'https://docs.github.com/' },
-  { title: 'Training', url: 'https://services.github.com/' },
-  { title: 'Status', url: 'https://www.githubstatus.com/' },
-  { title: 'Security', url: 'https://github.com/security' },
-  {
-    title: 'Terms',
-    url: 'https://docs.github.com/site-policy/github-terms/github-terms-of-service',
-  },
-  {
-    title: 'Privacy',
-    url: 'https://docs.github.com/site-policy/privacy-policies/github-privacy-statement',
-  },
-  { title: 'Docs', url: 'https://docs.github.com/' },
-];
 
 export default function Home() {
   const { data: session } = useSession({
@@ -61,55 +36,7 @@ export default function Home() {
       </aside>
       <div className="flex min-h-screen">
         {/* Left Sidebar */}
-        <aside
-          className="hidden w-1/4 border-r sm:block md:w-1/3"
-          style={{
-            backgroundColor: '#0d1117',
-            borderColor: '#30363d',
-            maxWidth: '350px',
-          }}
-        >
-          <div className="min-h-1/2 sticky bottom-0 top-0 z-30 flex max-h-screen flex-col overscroll-y-auto px-6 pt-4">
-            <div
-              className="mb-4 border-b py-4"
-              style={{ borderColor: '#21262d' }}
-            >
-              <button className="inline-block">
-                {name && image ? (
-                  <Image
-                    className="h-5 w-5 rounded-full"
-                    src={image}
-                    width={20}
-                    height={20}
-                    alt={name}
-                  />
-                ) : (
-                  <Icon name="user-circle" size={20} />
-                )}
-              </button>
-            </div>
-
-            <div className="min-h-[50%] flex-1">
-              <div>
-                <h2 className="mb-4 flex items-center justify-between text-base font-semibold md:text-sm">
-                  Top Repositories
-                  <Link
-                    className="btn bg-[#238636] px-3 py-1 text-white"
-                    href="#"
-                  >
-                    <Icon
-                      className="inline-block fill-current"
-                      name="book-outline"
-                      size={16}
-                    />{' '}
-                    New
-                  </Link>
-                </h2>
-                <UserTopRepos />
-              </div>
-            </div>
-          </div>
-        </aside>
+        <HomeLeftSidebar name={name} image={image} />
 
         {/* Main - Commit graphs */}
         <div className="w-2/3 flex-auto px-4 md:px-8">
@@ -151,49 +78,7 @@ export default function Home() {
                     <button onClick={() => signOut()}>Sign Out</button>
                   </div>
                 </section>
-                {/* Footer */}
-                <div className="mx-auto my-8 grid w-full max-w-none grid-cols-5 gap-4 px-0 text-xs">
-                  <div className="col-span-2">
-                    <Link
-                      className="flex items-center"
-                      href="https://github.com/"
-                    >
-                      <Icon
-                        name="github"
-                        className="fill-dark mr-2 dark:fill-white"
-                        size={24}
-                      />
-                      © 2023 GitHub, Inc.
-                    </Link>
-                  </div>
-                  <div className="">
-                    <ul className="pl-6">
-                      {footerLinks.slice(0, 5).map(({ title, url }) => (
-                        <li key={title} className="mb-1">
-                          <Link href={url}>{title}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="">
-                    <ul className="pl-6">
-                      {footerLinks.slice(5, 9).map(({ title, url }) => (
-                        <li key={title} className="mb-1">
-                          <Link href={url}>{title}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="">
-                    <ul className="pl-6">
-                      {footerLinks.slice(9).map(({ title, url }) => (
-                        <li key={title} className="mb-1">
-                          <Link href={url}>{title}</Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
+                <HomeFeedFooter />
               </div>
 
               {/* Right Sidebar */}
