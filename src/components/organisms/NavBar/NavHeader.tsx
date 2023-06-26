cd05b768936a4a04b2ef8ad8bb25e005d5e90115
morgnism <morgan@border.dev>
2023-06-25 19:12:38 -0600

    feat: update navbar header for repo page links

---
+++ src/components/organisms/NavBar/NavHeader.tsx
@@ -0,0 +1,69 @@
+import Link from '@components/modules/Link';
+import React from 'react';
+
+type NavHeaderProps = {
+  owner: string;
+  name: string;
+};
+
+const NavHeader = ({ owner, name }: NavHeaderProps) => {
+  return (
+    <>
+      <div className="md:hidden">
+        {owner && name ? (
+          <button className="dashboard-link grid h-12 w-full cursor-pointer rounded-md border-0 bg-transparent text-left text-[inherit] duration-[80ms] hover:bg-[#b1bac41f]">
+            <span className="Button--content">
+              <span className="Button--label">
+                <span className="grid w-fit grid-flow-col items-center text-xs text-[#7d8590]">
+                  {owner}
+                </span>
+                <strong className="flex min-w-0 max-w-full overflow-hidden text-ellipsis whitespace-nowrap">
+                  {name}
+                </strong>
+              </span>
+            </span>
+          </button>
+        ) : (
+          <Link
+            href="/"
+            className="flex items-center rounded-md duration-[80ms] hover:bg-[#b1bac41f]"
+          >
+            Dashboard
+          </Link>
+        )}
+      </div>
+      <div className="inline-flex w-full min-w-0 max-w-full overflow-hidden">
+        <nav aria-label="Page context" className="w-full">
+          <ul role="list" className="flex flex-row text-sm">
+            {owner && name ? (
+              <>
+                <li>
+                  <Link href={`/${owner}`} className="flex items-center">
+                    {owner}
+                  </Link>
+                  <span className="inline-grid grid-flow-col items-center">
+                    /
+                  </span>
+                </li>
+                <li>
+                  <Link href={`/${owner}/${name}`}>{name}</Link>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Link
+                  href="/"
+                  className="grid h-12 w-full cursor-pointer items-center rounded-md bg-transparent text-left text-sm font-semibold text-inherit duration-[80ms] hover:bg-[#b1bac41f]"
+                >
+                  Dashboard
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default NavHeader;
