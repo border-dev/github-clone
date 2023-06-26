4fab29c0fd75cae0fbb6f3d25dcd147b95d97109
morgnism <morgan@border.dev>
2023-06-25 20:39:07 -0600

    feat: added icons for repo page

---
+++ src/components/atoms/Icon/SVGs/code-issue.tsx
@@ -0,0 +1,24 @@
+import classNames from 'classnames';
+import { IconPropsDefaults } from '../icons-map';
+import { IconSVG } from '../types';
+
+export const CodeIssue: React.FC<IconSVG> = ({
+  className = IconPropsDefaults.className,
+  color = IconPropsDefaults.color,
+  size = IconPropsDefaults.size,
+}) => (
+  <svg
+    width={size}
+    height={size}
+    fill="none"
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+    className={classNames([className, color])}
+    focusable="false"
+    aria-hidden="true"
+  >
+    <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
+    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
+  </svg>
+);
