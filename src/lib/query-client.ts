5330c5b363628fba971e039f62d3f8d087de502f
morgnism <morgan@border.dev>
2023-06-30 18:49:14 -0600

    feat: added hydration component for query prefetching

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,10 +1,8 @@
-import { QueryClient } from '@tanstack/react-query';
-
-export const queryClient = new QueryClient({
+export const queryClientConfig = {
   defaultOptions: {
     queries: {
       staleTime: 5 * 1000,
       refetchOnWindowFocus: false,
     },
   },
-});
+};
